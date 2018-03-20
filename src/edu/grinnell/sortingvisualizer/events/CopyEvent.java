f8737eed754f6b0a7b6a428549d5107b140659ec
cuiqingq <35696035+cuiqingq@users.noreply.github.com>
2018-03-19 23:31:01 -0500

    Update CopyEvent.java

--- src/edu/grinnell/sortingvisualizer/events/CopyEvent.java
+++ src/edu/grinnell/sortingvisualizer/events/CopyEvent.java
@@ -14,18 +14,30 @@ public class CopyEvent<T> implements SortEvent<T>{
         this.value = value;
     }
     
+        
+    /**
+     * This method applies this sort event to the given list.
+     */
     public void apply(ArrayList<T> lst) {
         lst.set(index, value);
     }
 
+    /**
+     * This method returns a list containing all of the indices that this event affects
+	 * @return List, a list of integers
+     */
     public List<Integer> getAffectedIndices() {
         List<Integer> lst = new ArrayList<Integer>();
         lst.add(index);
         return lst;
     }
 
+    /**
+     * This method returns true if this event should be emphasized by the visualizer/audibilizer.
+     * @return boolean
+     */
     public boolean isEmphasized() {
         return true;
     }
     
-}
+    
