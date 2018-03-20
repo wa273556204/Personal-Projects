6e232d74c5ec7d8f32f5aea2302b7cc5f08c9200
cuiqingq <35696035+cuiqingq@users.noreply.github.com>
2018-03-19 23:32:02 -0500

    Update SwapEvent.java

--- src/edu/grinnell/sortingvisualizer/events/SwapEvent.java
+++ src/edu/grinnell/sortingvisualizer/events/SwapEvent.java
@@ -13,18 +13,28 @@ public class SwapEvent<T> implements SortEvent<T>{
         affectedIndices.add(y);
     }
     
-
+        
+    /**
+     * This method applies this sort event to the given list.
+     */
 	public void apply(ArrayList<T> lst) {
         T temp = lst.get(affectedIndices.get(0));
         lst.set(affectedIndices.get(0), 
         		lst.get(affectedIndices.get(1)));
         lst.set(affectedIndices.get(1), temp);
     }
-    
+     /**
+     * This method returns a list containing all of the indices that this event affects
+     * @return List, a list of integers
+     */
+	
     public List<Integer> getAffectedIndices() {
         return affectedIndices;
     }
-
+    /**
+     * This method returns true if this event should be emphasized by the visualizer/audibilizer.
+     * @return boolean
+     */
     public boolean isEmphasized() {
         return true;
     }
